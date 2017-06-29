a7b8a180ecf9e81bfbcec02bbb54186301efeb9c
kansepooja <kansepooja41@yahoo.com>
2017-06-29 19:31:11 +0530

    upload file
    login page

---
+++ Login Page.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+
+ <meta charset="utf-8">
+ 
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <meta name="description" content="">
+    
+  <meta name="author" content="">
+
+
+
+
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+ <link href="css/prod-mgmt.min.css" rel="stylesheet">
+
+  
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+ 
+ <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  
+
+   
+     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+     
+   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+ 
+<link rel = "stylesheet" type = "text/css" href = "myfile1.css" />
+</head>
+<body><!-- multistep form -->
+<div class="container">
+		<div class="row">
+			    <div class="col-sm-7 text"><img src="Wipro_Logo_RGB.png" height="100px" width="150px">
+				<h1 class="wow fadeInLeftBig animated" style="visibility: visible; animation-name: fadeInLeftBig;"> <strong>Product Management</strong></h1>
+				<div class="description wow fadeInLeftBig animated" style="visibility: visible; animation-name: fadeInLeftBig;">
+					<p>
+						Product Management System created by Wipro Technologies.
+					</p>
+				</div>
+				
+			</div>
+			<div class="col-sm-5 form-box wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp;">
+				<div class="form-top">
+					<div class="form-top-left">
+						<h3>Sign up now</h3>
+						<p>Fill in the form below to get instant access:</p>
+					</div>
+					<div class="form-top-right">
+						<span aria-hidden="true" class="typcn typcn-pencil"></span>
+					</div>
+				</div>
+				<div class="form-bottom">
+					<form role="form" action="" method="post">
+						<div class="form-group">
+							<label class="sr-only" for="form-first-name">First name</label>
+							<input type="text" name="form-first-name" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="form-last-name">Last name</label>
+							<input type="text" name="form-last-name" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="form-email">Email</label>
+							<input type="text" name="form-email" placeholder="Email..." class="form-email form-control" id="form-email">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="form-about-yourself">About yourself</label>
+							<textarea name="form-about-yourself" placeholder="About yourself..." class="form-about-yourself form-control" id="form-about-yourself"></textarea>
+						</div>
+						<button type="submit" class="btn">Sign me up!</button>
+						<div class="form-links">
+							<a href="#" class="launch-modal" data-modal-id="modal-privacy">Privacy Policy</a> - 
+							<a href="#" class="launch-modal" data-modal-id="modal-faq">FAQ</a>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
